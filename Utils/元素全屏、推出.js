f2ab5288c73ae6efde2a27c4bfe1c39913eeeec6
_MAYAWEN <mayawen@cdhengsu.com>
2020-06-12 22:02:41 +0800

    feat: New utils 元素全屏、退出.js --Myw

---
+++ Utils/元素全屏、推出.js
@@ -0,0 +1,28 @@
+/**
+ * ? 元素全屏/推出全屏。
+ * @func launchFullscreen 元素全屏化；
+ * @func exitFullscreen 推出全屏；
+ * @param { DOM } element DOM 节点对象。
+ */
+const launchFullscreen = element => {
+  if (element.requestFullscreen) {
+    element.requestFullscreen();
+  } else if (element.mozRequestFullScreen) {
+    element.mozRequestFullScreen();
+  } else if (element.msRequestFullscreen) {
+    element.msRequestFullscreen();
+  } else if (element.webkitRequestFullscreen) {
+    element.webkitRequestFullScreen();
+  }
+};
+const exitFullscreen = () => {
+  if (document.exitFullscreen) {
+    document.exitFullscreen();
+  } else if (document.msExitFullscreen) {
+    document.msExitFullscreen();
+  } else if (document.mozCancelFullScreen) {
+    document.mozCancelFullScreen();
+  } else if (document.webkitExitFullscreen) {
+    document.webkitExitFullscreen();
+  }
+};
